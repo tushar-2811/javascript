516aa72f2e584aef5c236f18720cf92f147ad44d
tushar-2811 <tusharrawat52@gmail.com>
2024-08-08 22:39:05 +0530

    crypto logic

--- test-backend/index2.js
+++ test-backend/index2.js
@@ -1,6 +1,27 @@
 import crypto from 'crypto';
 
-const input = "tushar";
-const hash = crypto.createHash('sha256').update(input).digest('hex');
+const input = "100xdevs";
 
-console.log(hash);
+
+
+function findStringWithPrefix(stringPrefix , hashPrefix){
+    let temp = 0;
+     
+
+     while(true){
+        let input =  stringPrefix + temp.toString();
+        const hash = crypto.createHash('sha256').update(input).digest('hex');
+        if(hash.startsWith(hashPrefix)){
+            return {
+                hash ,
+                input
+            }
+        }else{
+            temp++;
+        }
+     }
+}
+
+const ans = findStringWithPrefix('tushar => 100devx' , '000');
+console.log(ans.hash);
+console.log(ans.input);
